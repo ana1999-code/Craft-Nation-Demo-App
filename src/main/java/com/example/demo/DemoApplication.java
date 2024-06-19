5de94242bcf347063e81be84a0f7b862deed7184
Ana <anavlas1999@gmail.com>
2024-06-19 22:17:01 +0300

    Add docker-compose

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,9 +12,4 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-	@GetMapping("/")
-	public String greet(){
-		return "Hello";
-	}
 }
