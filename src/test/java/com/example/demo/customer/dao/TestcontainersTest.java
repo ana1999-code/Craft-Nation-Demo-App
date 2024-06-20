4b73006bbe9267313242b6177d233057d52fd0fb
Anisoara Vlasi <anisoara.vlasi@iongroup.com>
2024-06-20 15:05:12 +0300

    Add dynamic property source method that allow properties from Testcontainers based tests to be exposed easily to Spring integration tests.

--- src/test/java/com/example/demo/customer/dao/TestcontainersTest.java
+++ src/test/java/com/example/demo/customer/dao/TestcontainersTest.java
@@ -1,7 +1,9 @@
-package com.example.demo;
+package com.example.demo.customer.dao;
 
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -17,6 +19,24 @@ public class TestcontainersTest {
                     .withDatabaseName("dao-unit-test")
                     .withUsername("postgres")
                     .withPassword("12345");
+
+    @DynamicPropertySource
+    private static void registerDataSourceProperties(DynamicPropertyRegistry registry){
+        registry.add(
+                "spring.datasource.url",
+                postgreSQLContainer::getJdbcUrl
+        );
+
+        registry.add(
+                "spring.datasource.username",
+                postgreSQLContainer::getUsername
+        );
+
+        registry.add(
+                "spring.datasource.password",
+                postgreSQLContainer::getPassword
+        );
+    }
 
     @Test
     void canStartPostgresDB() {
