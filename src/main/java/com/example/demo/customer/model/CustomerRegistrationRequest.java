121194f3708808c2219acdaccbee1cdffeeb223d
Anisoara Vlasi <anisoara.vlasi@iongroup.com>
2024-06-21 10:02:12 +0300

    Remove unused annotations from customer models

--- src/main/java/com/example/demo/customer/model/CustomerRegistrationRequest.java
+++ src/main/java/com/example/demo/customer/model/CustomerRegistrationRequest.java
@@ -2,12 +2,10 @@ package com.example.demo.customer.model;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
--- src/main/java/com/example/demo/customer/model/CustomerUpdateRequest.java
+++ src/main/java/com/example/demo/customer/model/CustomerUpdateRequest.java
@@ -1,11 +1,9 @@
 package com.example.demo.customer.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
