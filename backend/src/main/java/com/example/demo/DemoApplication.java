c197d93397a8da4574d548640430bbd866d47ae1
Ana <anavlas1999@gmail.com>
2024-06-28 14:33:35 +0300

    Remove unused imports

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,8 @@
 package com.example.demo;
 
-import com.example.demo.customer.entity.Customer;
-import com.example.demo.customer.repository.CustomerRepository;
-import com.github.javafaker.Faker;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Random;
 
 @SpringBootApplication
 @RestController
